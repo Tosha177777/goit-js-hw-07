91025f8b0d54a75acf212f315dd4816d4020702f
Tosha177777 <slavkoanton4@gmail.com>
2023-07-21 15:29:32 +0300

    try 1

---
+++ js/01-gallery.js
@@ -0,0 +1,50 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const gallery = document.querySelector(".gallery");
+let largeImageUrl;
+let instance;
+
+const markup = galleryItems
+  .map(
+    ({ preview, description, original }) => `<li class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="large-image.jpg"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+gallery.addEventListener("click", modalClick);
+gallery.addEventListener("keydown", modalCloseEsc);
+
+function modalClick(e) {
+  e.preventDefault();
+  const target = e.target;
+  if (target === e.currentTarget) {
+    return;
+  }
+  largeImageUrl = target.parentNode.href;
+  modalOpen();
+}
+
+function modalOpen() {
+  instance = basicLightbox.create(
+    `
+		<img src="${largeImageUrl}">
+	`
+  );
+  instance.show();
+}
+
+function modalCloseEsc(e) {
+  if (e.code === "Escape" && instance) {
+    instance.close();
+  }
+}
---
+++ js/02-lightbox.js
@@ -0,0 +1,39 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const gallery = document.querySelector(".gallery");
+let largeImageUrl;
+
+const markup = galleryItems
+  .map(
+    ({ preview, description, original }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", markup);
+
+gallery.addEventListener("click", modalClick);
+
+function modalClick(e) {
+  e.preventDefault();
+  const target = e.target;
+  if (target === e.currentTarget) {
+    return;
+  }
+  modalSlider();
+}
+
+function modalSlider() {
+  let lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionPosition: "bottom",
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+  return lightbox;
+}
+
+console.log(galleryItems);
